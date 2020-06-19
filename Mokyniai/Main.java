b369f01c08038dc5889f0d0e40499378a00060c9
Klaudijus <klaudijus.k1996@gmail.com>
2020-06-19 12:11:49 +0300

    test

--- Mokyniai/Main.java
+++ Mokyniai/Main.java
@@ -3,41 +3,56 @@ import java.util.*;
 
 public class Main {
 
-    static int mokiniuSkaicius = 5;
+    static int mokiniuSkaicius = 100000;
     static Mokinys[] mokiniai = new Mokinys[mokiniuSkaicius];
 
     public static void main(String[] args) {
-        /*** Generating random string ***/
-        Random r = new Random();
-        String abc = "abcdefghijklmnoprstuxw";
-        String vardenis = "";
-        for (int i=0;i<10;i++) {
-            vardenis += abc.charAt(r.nextInt(abc.length()));
-        }
-        System.out.println("ar gavosi? "+vardenis);
-        System.out.println("duok raide? "+vardenis.charAt(0));
-        /*** Generating random string END ***/
-        ivestiPradiniusDuomenis();
-//        print("Pradiniai duomenys:");
-//
-//        pagalKlase();
-//        print("Sulygiuota pagal klase:");
-//
-//        pagalVarda();
-//        print("Lygiuot pagal varda:");
-//
-//        pagalKlasePavardeVarda();
-//        print("Lygiuot pagal klase, pavarde ir varda");
-//
-//        pagalVardaPavardeKlase();
-//        print("Lygiuot pagal varda, pavarde ir klase");
+        /*** Iskvieciam 'mokiniu.length() generavima' ***/
+        vykstaDalykai(mokiniai);
 
+        long startTime = System.currentTimeMillis();
+//        pagalKlasePavardeVarda();
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Kiek ms trunka atlikti rusiavima nenaudojant .sort(): ");
+        System.out.println((stopTime - startTime)); // jeigu .length() > 100, tai lauksim nuo 870 ms.
+        long startNewTime= System.currentTimeMillis();
+        pagalVardaPavardeKlase();
+        long stopNewTime = System.currentTimeMillis();
+        System.out.println("Kiek ms trunka atlikti rusiavima naudojant .sort(): ");
+        System.out.println(stopNewTime - startNewTime); // jeigu .length() <== 100 000 , tai lauksim 400 ms++
 
         System.out.println("\nDemo spausdinimas: " + Arrays.toString(mokiniai));
     }
-
-
-    static void ivestiPradiniusDuomenis() {
+    /*** Generating Random Grade ***/
+    static int randomGrade() {
+        int klase = 0;
+        for (int i=0;i<=12;i++) {
+            klase = (int)(Math.random()*12);
+        }
+        return klase;
+    }
+    /*** Generating Random Name ***/
+    static String randomName() {
+        Random r = new Random();
+        String abc = "abcdefghijklmnoprstuxw";
+        String vardenis = "";
+        for (int i=0;i<5;i++) {
+            vardenis += abc.charAt(r.nextInt(abc.length()));
+        }
+        return vardenis;
+    }
+    /*** Generating Random Lastname ***/
+    static String randomLastName() {
+        Random r = new Random();
+        String abc = "abcdefghijklmnoprstuxw";
+        String pavardenis = "";
+        for (int i=0;i<7;i++) {
+            pavardenis += abc.charAt(r.nextInt(abc.length()));
+        }
+        return pavardenis;
+    }
+    /*** Generating object ***/
+    static Mokinys[] vykstaDalykai(Mokinys[] mokiniai){
         Mokinys mokinys1 = new Mokinys();
         mokinys1.setKlase(1);
         mokinys1.setVardas("Jonas");
@@ -67,100 +82,52 @@ public class Main {
         mokinys5.setVardas("Antanas");
         mokinys5.setPavarde("Antanaitis");
         mokiniai[4] = mokinys5;
-        System.out.println(Arrays.toString(mokiniai));
+        for (int i=5;i<mokiniai.length;i++){
+            Mokinys mokinys = new Mokinys();
+            mokinys.setKlase(randomGrade());
+            mokinys.setVardas(randomName());
+            mokinys.setPavarde(randomLastName());
+            mokiniai[i] = mokinys;
+        }
+        return mokiniai;
     }
+    static void sukeiciamMokinius(int i, int j) {
+        Mokinys mokinys = mokiniai[i];
+        mokiniai[i] = mokiniai[j];
+        mokiniai[j] = mokinys;
+    }
+    /*** Sorting without using .sort() ***/
+    static void pagalKlasePavardeVarda() {
+        for (int zingsnis = 0; zingsnis < vykstaDalykai(mokiniai).length - 1; zingsnis++) {
 
-//    static void print(int indeksas) {
-//        System.out.println(mokiniai[indeksas].getKlase() + " " +
-//                mokiniai[indeksas].vardas + " " +
-//                mokiniai[indeksas].pavarde);
-//    }
-//
-//    static void print(String title) {
-//        System.out.println();
-//        System.out.println(title);
-//        for (int i = 0; i < mokiniai.length; i++) {
-//            print(i);
-//        }
-//    }
+            for (int i = zingsnis; i < mokiniai.length; i++) {
 
+                if (vykstaDalykai(mokiniai)[i].getKlase() < vykstaDalykai(mokiniai)[zingsnis].getKlase()) {
+                    sukeiciamMokinius(i, zingsnis);
 
+                } else if (vykstaDalykai(mokiniai)[i].getKlase() == vykstaDalykai(mokiniai)[zingsnis].getKlase()) {
+                    if (vykstaDalykai(mokiniai)[i].getPavarde().compareTo(vykstaDalykai(mokiniai)[zingsnis].getPavarde()) < 0) {
+                        sukeiciamMokinius(i, zingsnis);
 
-    /*
-     indeksas: 0  1  2 3 4 5
-     korta:    4  8  5 7 1 2
-     0:        1* 8  5 7 4 2
-     1:        1  2* 8 7 5 4
-     0. Turime is kortu nuo 0 iki 5 isrinkti maziausia ir padeti ja i 0 (!) pozicija
-     1. Turime is kortu nuo 1 iki 5 isrinkti maziausia ir padeti ja i 1 pozicija
-     ...
-     n. Turime is kortu nuo n iki 5 isrinkti maziausia ir padeti ja i n pozicija
-     */
-/*** -------------- ***/
+                    } else if (vykstaDalykai(mokiniai)[i].getPavarde().compareTo(vykstaDalykai(mokiniai)[zingsnis].getPavarde()) == 0) {
+                        if (vykstaDalykai(mokiniai)[i].getVardas().compareTo(vykstaDalykai(mokiniai)[zingsnis].getVardas()) < 0) {
+                            sukeiciamMokinius(i, zingsnis);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    /*** Sorting using .sort() ***/
+    static void pagalVardaPavardeKlase() {
+        Arrays.sort(mokiniai, (m1, m2) -> {
+            int pagalVarda = m1.getVardas().compareTo(m2.getVardas());
+            if (pagalVarda != 0) return pagalVarda;
 
-//    static void sukeiciamMokinius(int i, int j) {
-//        Mokinys mokinys = mokiniai[i];
-//        mokiniai[i] = mokiniai[j];
-//        mokiniai[j] = mokinys;
-//    }
-//
-//    static void pagalKlase() {
-//        for (int zingsnis = 0; zingsnis < mokiniai.length - 1; zingsnis++) {
-//
-//            for (int i = zingsnis; i < mokiniai.length; i++) {
-//
-//                if (mokiniai[i].klase < mokiniai[zingsnis].klase) {
-//                    // mokiniai[i] <-> mokiniai[zingsnis]
-//                    sukeiciamMokinius(i, zingsnis);
-//                }
-//            }
-//        }
-//    }
-//
-//    static void pagalVarda() {
-//        for (int zingsnis = 0; zingsnis < mokiniai.length - 1; zingsnis++) {
-//
-//            for (int i = zingsnis; i < mokiniai.length; i++) {
-//
-//                if (mokiniai[i].vardas.compareTo(mokiniai[zingsnis].vardas) < 0) {
-//                    // mokiniai[i] <-> mokiniai[zingsnis]
-//                    sukeiciamMokinius(i, zingsnis);
-//                }
-//            }
-//        }
-//    }
-//
-//    static void pagalKlasePavardeVarda() {
-//        for (int zingsnis = 0; zingsnis < mokiniai.length - 1; zingsnis++) {
-//
-//            for (int i = zingsnis; i < mokiniai.length; i++) {
-//
-//                if (mokiniai[i].klase < mokiniai[zingsnis].klase) {
-//                    sukeiciamMokinius(i, zingsnis);
-//
-//                } else if (mokiniai[i].klase == mokiniai[zingsnis].klase) {
-//                    if (mokiniai[i].pavarde.compareTo(mokiniai[zingsnis].pavarde) < 0) {
-//                        sukeiciamMokinius(i, zingsnis);
-//
-//                    } else if (mokiniai[i].pavarde.compareTo(mokiniai[zingsnis].pavarde) == 0) {
-//                        if (mokiniai[i].vardas.compareTo(mokiniai[zingsnis].vardas) < 0) {
-//                            sukeiciamMokinius(i, zingsnis);
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    static void pagalVardaPavardeKlase() {
-//        Arrays.sort(mokiniai, (m1, m2) -> {
-//            int pagalVarda = m1.vardas.compareTo(m2.vardas);
-//            if (pagalVarda != 0) return pagalVarda;
-//
-//            int pagalPavarde = m1.pavarde.compareTo(m2.pavarde);
-//            if (pagalPavarde != 0) return pagalPavarde;
-//
-//            return Integer.compare(m1.klase, m2.klase);
-//        });
-//    }
+            int pagalPavarde = m1.getPavarde().compareTo(m2.getPavarde());
+            if (pagalPavarde != 0) return pagalPavarde;
+
+            return Integer.compare(m1.getKlase(), m2.getKlase());
+        });
+    }
 }
